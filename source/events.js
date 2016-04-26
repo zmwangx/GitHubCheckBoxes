b1cb0e8dca89a299bfcdd6b73714a3f0abaf5493
Zhiming Wang <zmwangx@gmail.com>
2016-04-26 00:08:17 -0700

    Switch to page action (previously browser action)
    More suitable for this extension which only takes effect on GitHub.

--- source/events.js
+++ source/events.js
@@ -1,3 +1,10 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
+// Show page action only for a GitHub page
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (tab.url.match(/^https?:\/\/github.com/)) {
+    chrome.pageAction.show(tabId)
+  }
+})
+
+chrome.pageAction.onClicked.addListener(function (tab) {
   chrome.tabs.sendMessage(tab.id, 'check')
 })
